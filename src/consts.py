695c6a9427eb9618374858f05d20a7d1ca3a24ce
KesavaViswanadha <79319318+KesavaViswanadha@users.noreply.github.com>
2024-03-01 22:03:15 -0800

    Update consts.py to support the TASK_LIST being moved

--- src/consts.py
+++ src/consts.py
@@ -2,4 +2,13 @@ import torch
 
 
 SEQ_MODELS = ["gpt2", "lstm", "relu_attn", "relu_attn_causal"]
+TASK_LIST = [
+    "linear_regression",
+    "sparse_linear_regression",
+    "linear_classification",
+    "relu_2nn_regression",
+    "decision_tree",
+    "kalman_filter",
+    "noisy_kalman_filter"
+]
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
