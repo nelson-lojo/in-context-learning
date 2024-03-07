235129381265569d7675f0d21f70cbd947ba1e71
Nelson Lojo <31297176+nelson-lojo@users.noreply.github.com>
2024-03-07 10:07:37 -0800

    MLP to mimic current SCuM controller (#7)
    - added parametrized mlp model with variable context length, layer widths, layer count, and activation function
    - reworked sequence model schema validation to now use optional fields and delegate argument null-checking to model instantiation
    - added model and experiment configs for some MLP example models
    - minor additions to .gitignore -- IDE artifacts
    
    ---------
    
    Co-authored-by: OnBlockHocTaco <aviralavnimishra25@gmail.com>
    Co-authored-by: DerrickhSun <49956063+DerrickhSun@users.noreply.github.com>

--- src/consts.py
+++ src/consts.py
@@ -1,5 +1,10 @@
 import torch
 
 
-SEQ_MODELS = ["gpt2", "lstm", "relu_attn", "relu_attn_causal"]
+SEQ_MODELS = ["gpt2", "lstm", "relu_attn", "relu_attn_causal", "mlp"]
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def NULL_CHK(*values):
+    for i, val in enumerate(values):
+        if val is None:
+            raise ValueError(f"Encountered `None` value in argument {i+1}!")
